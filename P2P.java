8964c9a429eb76c09f01e63ed3fa926b17914e95
Alaine Spade <spadea@ufl.edu>
2023-10-24 15:46:19 -0400

    moved peer into its own file

--- P2P.java
+++ P2P.java
@@ -4,22 +4,6 @@ import java.util.ArrayList;
 // general container for the P2P process
 public class P2P
 {
-    // container for peer information
-    public class Peer
-    {
-        int peerID;
-        String hostName;
-        int portNumber;
-        boolean hasFile;
-
-        Peer(int peerID, String hostName, int portNumber, boolean hasFile)
-        {
-            this.peerID = peerID;
-            this.hostName = hostName;
-            this.portNumber = portNumber;
-            this.hasFile = hasFile;
-        }
-    }
 
     // information gathered from Common.cfg, PeerInfo.cfg
     int numPreferredNeighbors;
